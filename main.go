e3a769f6a3ecb063beee9a781f5abdfabbadc57f
kidiga <juniorprogrammer09@gmail.com>
2024-11-22 02:01:04 +0300

    CRUD done(creates, reads, updates and deletes the todos as expected

--- main.go
+++ main.go
@@ -7,25 +7,68 @@ import (
 
 func main() {
 
-	//for databases
-	// newTodo := todo.Todo{
-	// 	Id: 1,
-	// 	Title: "Hello todo",
-	//  	Message: "Hello I was created.",
-	// 	IsDone: false,
-	// }
-
-	//close db after script close
-	defer func(){
-		todo.GetDb().Close()
+	//close db before script close
+	defer func ()  {
+		todo.CloseDb()
 	}()
 
-	// todo.CreateTodoTable()
-	// todo.InsertSingleTodo(&newTodo)
+
+	todo.CreateTodoTable()
+	todo.InsertSingleTodo(
+		& todo.Todo{
+		Id: 1,
+		Title: "Hello todo 1",
+	 	Message: "Hello I was created. first",
+		IsDone: false,
+	})
+
+	todo.InsertSingleTodo(
+		& todo.Todo{
+		Id: 2,
+		Title: "Hello todo 2",
+	 	Message: "Hello I was created. second",
+		IsDone: false,
+	})
+
+	//fetching all todos
 	var todos []todo.Todo
 	todo.FetchTodos(&todos)
-	fmt.Println(todos)
+	fmt.Println("ALL : ",todos)
+
+	//fetching done todos
+	var doneTodos []todo.Todo
+	todo.FetchDoneTodos(&doneTodos)
+	fmt.Println("DONE : ",doneTodos)
 	
-	todo.DeleteAllTodos()
+	//updating todos
+	forUpdates := &todo.Todo{Title: "Hello todo 1", IsDone: true}
+	todo.UpdateTodoStatus(forUpdates)
+	todo.UpdateTodoMessage(forUpdates, "Fuck lorem ispum.")
+	todo.UpdateTodoTitle(forUpdates, "I am for lorem ipsum.")
+
+	//more updates
+	var updatedTodos []todo.Todo
+	todo.FetchDoneTodos(&updatedTodos)
+	fmt.Println("UPDATED : ",updatedTodos)
+	fmt.Println(todo.CheckTodoExists(&todo.Todo{Title: "Hello todo 1"}))
+
+	todo.InsertSingleTodo(&todo.Todo{
+		Title: "I will be deleted.", 
+		Message: "But why",
+		IsDone: true,
+	})
+	todo.DeleteTodoByTitle("I will be deleted.")
+	
+	var afterDelete []todo.Todo
+	todo.FetchTodos(&afterDelete)
+	fmt.Println("AFTER DELETING : ",updatedTodos)
+
+	//clearing db
+	todo.ClearDoneTodos()
+	var notDoneTodos []todo.Todo
+	todo.FetchTodos(&notDoneTodos)
+	fmt.Println("NOT DONE : ",notDoneTodos)
+
+	// todo.DeleteAllTodos()
 }
 
