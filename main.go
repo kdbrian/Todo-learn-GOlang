a98ed5f467a46a195cb7cb98e51c6226ae5e69d4
kidiga <juniorprogrammer09@gmail.com>
2024-11-21 22:25:37 +0300

    functions to operate on todo

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
+import "fmt"
 
-type Todo struct{
-	title, message string
-	isDone bool
+type Todo struct {
+	Title, Message string
+	IsDone         bool
 }
 
 func main() {
 
-	
+	todo := Todo{}
+	todo.Title = "Hello world"
+	todo.Message = "Hello world this is a message."
+	todo.IsDone = false
+	fmt.Println(todo)
 
+	todoPtr := &todo
+	//updating status
+	updateTodoStatus(todoPtr)
+	fmt.Println(todo)
+
+	updateTitle(todoPtr, "Some change title")
+	fmt.Println(todo)
+
+	updateMessage(todoPtr,"I changed the title.")
+	fmt.Println(todo)
+
+}
+
+func updateTitle(todo *Todo, newTitle string){
+	todo.Title = newTitle
+}
+func updateMessage(todo *Todo, message string){
+	todo.Message = message
+}
+
+func updateTodoStatus(todo *Todo){
+	todo.IsDone = !todo.IsDone
 }
